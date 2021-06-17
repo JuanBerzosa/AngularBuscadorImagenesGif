cbe1d81e10eb2950d485467a4eb44350562fcb8d
Juan Berzosa <getinfo@ono.com>
2021-06-17 08:35:04 +0200

    Añadir una homepage al componente de gifs

---
+++ src/app/gifs/gif-home-page/gif-home-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-home-page',
+  templateUrl: './gif-home-page.component.html',
+  styles: [
+  ]
+})
+export class GifHomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
